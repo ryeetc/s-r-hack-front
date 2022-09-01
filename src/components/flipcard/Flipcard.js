a3e481a5e04db6efc48a00ab1c6c4c8f5be9a7e8
ryeetc <rclancaster1@gmail.com>
2022-09-01 12:12:37 -0400

    add styling

--- src/components/flipcard/Flipcard.js
+++ src/components/flipcard/Flipcard.js
@@ -1,18 +1,44 @@
 import "./Flipcard.scss"
+import { useState, useEffect } from "react"
+import axios from "axios"
 
 const Flipcard = () => {
+
+    const [movieCards, setMovieCards] = useState(null)
+
+    useEffect(()=>{
+        const videos = axios.get("http://localhost:8080/all")
+        videos
+            .then((response)=>{
+                setMovieCards(response.data)
+            })
+        
+    },[])
+
+    if(!movieCards) {
+        return (
+            <p>Loading . . .</p>
+        )
+    }
+    
+    
     return (
         <div className="flip-card">
             <div className="flip-card-inner">
                 <div className="flip-card-front">
+                    <img src={`${movieCards[Math.floor(Math.random()* 250)].image}`} alt="Avatar" className="flip__image"></img>
                 </div>
                 <div className="flip-card-back">
-                    <img src="" alt="Avatar" className="flip__image"></img>
+                    <img src={`${movieCards[Math.floor(Math.random()* 250)].image}`} alt="Avatar" className="flip__image"></img>
                 </div>
             </div>
         </div>
 
+        
     )
+    
 }
+
+export default Flipcard
 
 
