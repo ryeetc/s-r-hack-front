a75b37b544f82ea37155c042ba965eee018617e2
ryeetc <rclancaster1@gmail.com>
2022-09-01 09:49:00 -0400

    styling

---
+++ src/components/flipcard/Flipcard.js
@@ -0,0 +1,18 @@
+import "./Flipcard.scss"
+
+const Flipcard = () => {
+    return (
+        <div className="flip-card">
+            <div className="flip-card-inner">
+                <div className="flip-card-front">
+                </div>
+                <div className="flip-card-back">
+                    <img src="" alt="Avatar" className="flip__image"></img>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+
--- src/pages/home.js
+++ src/pages/home.js
@@ -8,6 +8,15 @@ const Home = () => {
             <h1 className="main__title">Welcome To BestFlix</h1>
             <p className="main__desc">Guess the movie with the higher review score</p>
             <Link to="/game" className="main__link">Click Here To Play</Link>
+            <div className="flip-card">
+                <div className="flip-card-inner">
+                    <div className="flip-card-front">
+                    </div>
+                    <div className="flip-card-back">
+                        <img src="" alt="Avatar" className="flip__image"></img>
+                    </div>
+                </div>
+            </div>
         </main>
     )
 }
